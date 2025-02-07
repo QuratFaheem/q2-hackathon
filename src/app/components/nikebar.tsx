3ead010394990cc22ff924de4db1a42022b0b7fd
QuratFaheem <mrs.faheem16@gmail.com>
2025-02-07 08:58:44 +0500

    Done

--- src/app/components/nikebar.tsx
+++ src/app/components/nikebar.tsx
@@ -8,15 +8,12 @@ export default function Nikebar() {
       {/* Logo Section */}
       <div className="logo">
         <Link href="/">
-          <Image src="/vector.png" alt="Logo" width={30} height={30} />
+          <Image src="/Vector.png" alt="Logo" width={30} height={30} />
         </Link>
       </div>
 
       {/* Navigation List */}
       <ul className="hidden md:flex items-center space-x-6 flex-grow justify-center">
-        <li>
-          <Link href="/noproduct" className="text-lg text-black">News & Featured</Link>
-        </li>
         <li>
           <Link href="/menshoes" className="text-lg text-black">Men</Link>
         </li>
@@ -34,16 +31,16 @@ export default function Nikebar() {
         </li>
       </ul>
 
-      {/* Search & Cart */}
-      <div className="flex items-center space-x-2">
-        <input
-          type="text"
-          placeholder="Search"
-          className="rounded px-2 py-1 border border-gray-300"
-        />
-        <Image src="/s2.png" alt="Search Icon" width={50} height={50} />
+      {/* Cart Icon */}
+      <div className="flex items-center">
         <Link href="/cart">
-          <Image src="/s3.png" alt="Cart Icon" width={50} height={50} />
+          <Image 
+            src="/s3.png" 
+            alt="Cart Icon" 
+            width={50} 
+            height={50} 
+            className="w-14 h-14 sm:w-16 sm:h-16 md:w-18 md:h-18"
+          />
         </Link>
       </div>
     </nav>
